3b438475f244131c5146a55cde14de343f9b9538
Sandesh Tiwari <sandeshtiwari@live.com>
2018-08-20 22:45:36 -0500

    feat: Fit the training sets

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -17,5 +17,6 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
 # Fitting Simple Linear Regression to the Training set
-from sklearn.model_selection import LinearRegression
+from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
+regressor.fit(X_train, y_train)
